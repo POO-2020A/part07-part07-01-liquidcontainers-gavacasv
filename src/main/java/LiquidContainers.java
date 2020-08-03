504a2a1d769aba2c95e18f7b7e8cc18630b53aba
gavacasv <58041959+gavacasv@users.noreply.github.com>
2020-08-03 16:09:24 -0500

    Add files via upload

--- src/main/java/LiquidContainers.java
+++ src/main/java/LiquidContainers.java
@@ -5,17 +5,44 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int first=0,second=0;
+        int amount;
 
 
         while (true) {
-            System.out.print("> ");
+
+            System.out.println("First: "+first+"/100");
+            System.out.println("Second: "+second+"/100");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
+            String[] partes=input.split(" ");
+            amount=Integer.valueOf(partes[1]);
+            switch (partes[0]){
+                case "add":
+                    if(first+amount>100)
+                        first=100;
+                    else
+                        first+=amount;
+                 break;   
+                case "remove":
+                    if(second-amount<0)
+                        amount=second;
+                    second-=amount;
+                 break;   
+                case "move":
+                    if(first-amount<0 || first==0)
+                        amount=first;
+                    second+=amount; 
+                    first-=amount;
+                 break;   
+            }
+            
 
         }
     }
+   
 
 }
