7bb1fc758d12fe385f4546f66cd0ea3eed506a03
gavacasv <58041959+gavacasv@users.noreply.github.com>
2020-08-03 16:34:50 -0500

    Add files via upload

--- src/main/java/LiquidContainers.java
+++ src/main/java/LiquidContainers.java
@@ -20,25 +20,35 @@ public class LiquidContainers {
             }
             String[] partes=input.split(" ");
             amount=Integer.valueOf(partes[1]);
-            switch (partes[0]){
-                case "add":
-                    if(first+amount>100)
-                        first=100;
-                    else
-                        first+=amount;
-                 break;   
-                case "remove":
-                    if(second-amount<0)
-                        amount=second;
-                    second-=amount;
-                 break;   
-                case "move":
-                    if(first-amount<0 || first==0)
-                        amount=first;
-                    second+=amount; 
-                    first-=amount;
-                 break;   
+            if(amount >0){
+                switch (partes[0]) {
+                    case "add":
+                        if (first + amount > 100) {
+                            first = 100;
+                        } else {
+                            first += amount;
+                        }
+                        break;
+                    case "remove":
+                        if (second - amount < 0) {
+                            amount = second;
+                        }
+                        second -= amount;
+                        break;
+                    case "move":
+                        if (first - amount < 0 || first == 0) {
+                            amount = first;
+                        }
+                        if (second + amount > 100) {
+                            second = 100;
+                        } else {
+                            second += amount;
+                        }
+                        first -= amount;
+                        break;
+                } 
             }
+            
             
 
         }
